473be82a43d51e9879864e5e3be4bf40140714be
wends05 <wendellterence.dador-23@cpu.edu.ph>
2025-06-18 13:25:58 +0800

    updated simulation page

--- src/routes/(authorized)/simulation/$scenarioId.tsx
+++ src/routes/(authorized)/simulation/$scenarioId.tsx
@@ -1,10 +1,11 @@
-import { createFileRoute } from '@tanstack/react-router'
-import { Simulation } from '@/components/Simulation'
+import { createFileRoute } from "@tanstack/react-router";
+import { Simulation } from "@/components/Simulation";
 
-export const Route = createFileRoute('/(authorized)/simulation/$scenarioId')({
-  component: () => {
-    const { scenarioId } = Route.useParams()
-    // do something with the scenario id
-    return <Simulation scenarioId={scenarioId} />
-  },
-})
+export const Route = createFileRoute("/(authorized)/simulation/$scenarioId")({
+  component: SimulationPage,
+});
+
+function SimulationPage() {
+  const { scenarioId } = Route.useParams();
+  return <Simulation scenarioId={scenarioId} />;
+}
